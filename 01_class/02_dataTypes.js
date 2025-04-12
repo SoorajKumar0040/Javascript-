334aff95f660684a1dbca8c3cf09f580432c8ece
SoorajKumar0040 <156647185+SoorajKumar0040@users.noreply.github.com>
2025-04-12 16:27:11 +0000

    dataTypes config done

---
+++ 01_class/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all js code as new verion
+
+// alert (3+3) we are using node.js not broswer
+
+console.log(3
+    +3)    
+        //code have readabilty should be high 
+
+console.log("suraj")
+
+
+let name = "suraj"
+let age  = 20;
+let boolean = true;
+let state;
+
+// int = 2 power 53
+//bigint = big data than int
+//string = "" name,place or anything
+//boolean = true/false
+//null = standlone value/
+//undefined = vaule no initailize/assign
+//symbol = unique
+
+console.log(typeof null); //Object
+console.log(typeof undefined); //undefined
