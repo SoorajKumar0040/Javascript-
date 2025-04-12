d038e5ebf34078931b0af5dd4795a13849ae68e1
SoorajKumar0040 <156647185+SoorajKumar0040@users.noreply.github.com>
2025-04-12 10:10:48 +0000

    Config variable done

---
+++ 01_class/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 6789
+let accountEmail = "soorraj8872@gamil.com"
+var accountPasssword = 12345;
+accountCity = "Ludhiana" //this is worng way to code 
+
+//accountId = 2 //Not allowed because of const variable
+accountEmail = "suraj6568@gmail.com";
+accountPasssword = 67890;
+accountCity = "chandigarh"
+
+// Not using var 
+// because inblock scope and function scope
+
+console.table([accountId, accountEmail,accountPasssword,accountCity]);
