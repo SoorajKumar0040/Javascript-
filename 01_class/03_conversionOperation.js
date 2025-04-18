9d9b180a81aa146fd01ced4b3d7f67d18d439c62
SoorajKumar0040 <156647185+SoorajKumar0040@users.noreply.github.com>
2025-04-18 09:30:03 +0000

    ConversionOperation Done

--- 01_class/03_conversionOperation.js
+++ 01_class/03_conversionOperation.js
@@ -3,9 +3,9 @@ let score = "33abc";
 //console.log(typeof score);
 //console.log(typeof (score));
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"33" = 33
 //"33abc" = NaN (Not a Number)
@@ -13,10 +13,53 @@ console.log(valueInNumber);
 
 let isLoggedIn = "hitesh";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 = true; 0 = false;
+// "" = false;
+// "hitesh" = true;
 
 let someNumber = 33
-let stringNumber = String (someNumber)
-console.log(stringNumber);
+// let stringNumber = String (someNumber)
+// console.log(stringNumber);
+
+//***************************** Operations **********************
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "Suraj"
+let str2 = "Kumar"
+// let str3 = str1+ str2
+// console.log(str3);
+
+// //Problem
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2"); 
+// //above line is not good coding skill instead of this
+// console.log ( (3 + 4) * 5 % 3);
+ 
+// console.log(true);
+// console.log(+true);
+
+let num3 = num2 = num1 = 2+2
+console.log(num3);
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
 
